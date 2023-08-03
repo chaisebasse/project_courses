1914e10a2cf5d6af190abfa92951ecebc55ad15f
Ermanno di Giulio <digiulioermanno@gmail.com>
2023-08-03 13:07:24 +0200

    changed course_sku to order_sku in migration file

--- db/migrate/20230726140351_create_orders.rb
+++ db/migrate/20230726140351_create_orders.rb
@@ -2,7 +2,7 @@ class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     create_table :orders do |t|
       t.string :state
-      t.string :course_sku
+      t.string :order_sku
       t.monetize :amount
       t.string :checkout_session_id
       t.references :user, null: false, foreign_key: true
--- db/migrate/20230802134620_rename_course_sku_to_order_sku.rb
+++
@@ -1,5 +0,0 @@
-class RenameCourseSkuToOrderSku < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :orders, :course_sku, :order_sku
-  end
-end
