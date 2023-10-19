e1347dd47fd19608212a0fffec26aadea052ec06
Ermanno di Giulio <digiulioermanno@gmail.com>
2023-10-19 18:58:28 +0200

    Adding Modal pt1

--- app/javascript/controllers/course_sections_controller.js
+++ app/javascript/controllers/course_sections_controller.js
@@ -3,12 +3,8 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="course-sections"
 export default class extends Controller {
   static targets = ["sectionsList"];
-  connect() {
-    console.log("Yello")
-  }
 
   toggleSections() {
-    console.log("ok")
     this.sectionsListTarget.classList.toggle("hidden");
   }
 }
